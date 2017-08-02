e7478aa50e776e23f6587d47654641325d1a1d9d
nakelia <nakelia78@gmail.com>
2017-08-02 14:26:55 -0400

    Second commit

---
+++ src/main/java/me/kikorose/demo/TestingFormApplication.java
@@ -0,0 +1,12 @@
+package me.kikorose.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingFormApplication.class, args);
+	}
+}
---
+++ src/test/java/me/kikorose/demo/TestingFormApplicationTests.java
@@ -0,0 +1,16 @@
+package me.kikorose.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestingFormApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
